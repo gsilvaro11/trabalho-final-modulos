b03ce5e66ee508f9dc1861d540ccd845b0c56a14
gsilvaro11 <guiuser1999@gmail.com>
2021-10-16 08:47:23 -0300

    Adicionando Classe Contato

---
+++ src/com/dbc/Contato.java
@@ -0,0 +1,24 @@
+package com.dbc;
+
+public class Contato {
+    private String email;
+    private String telefone;
+
+    Contato(){};
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
