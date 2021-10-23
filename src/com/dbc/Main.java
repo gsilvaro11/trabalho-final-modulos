5afd6bf3b6274f159a2a852c5321e2bebdefb4a6
devcamilo1995 <matheus.camilo1617@gmail.com>
2021-10-23 15:22:25 -0300

    Commit Reserva e Hoteis2

--- src/com/dbc/Main.java
+++ src/com/dbc/Main.java
@@ -157,6 +157,7 @@ public class Main {
                     hoteisService.listarHoteis();
                     System.out.println("Digite o índice do Hotel: ");
                     Integer indexHotel = scanner.nextInt();
+                    reservaService.removerReservaPorHotel(indexHotel);
                     hoteisService.removerHotel(indexHotel);
                     break;
                 }
@@ -168,18 +169,6 @@ public class Main {
 
 
 
-//                    quartosService.listarQuartosPorHotel(indexHotel);
-//                    System.out.println("Digite o índice do Quarto ");
-//                    Integer indexQuarto = scanner.nextInt();
-//                    quartos.setIdQuarto(indexQuarto);
-//                    hoteis.setIdHotel(indexHotel);
-//                    usuario.setIdUsuario(1);
-//
-//                    reserva.setHoteis(hoteis);
-//                    reserva.setQuartos(quartos);
-//                    reserva.setUsuario(usuario);
-//                    reserva.setDataReserva(LocalDate.parse(dataLocal, formatter));
-//                    reservaService.adicionarReserva(reserva);
 
 
 
